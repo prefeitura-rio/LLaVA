7d40fba39c932df2a92c785c4d44d09946b0ef87
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-11-04 00:48:50 +0200

    Added timer

--- llava/serve/test_api.py
+++ llava/serve/test_api.py
@@ -1,9 +1,22 @@
 import json
+import time
 import requests
 import base64
 
 
 BASE_URI = 'https://example.com'
+
+
+class Timer:
+    def __init__(self):
+        self.start = time.time()
+
+    def restart(self):
+        self.start = time.time()
+
+    def get_elapsed_time(self):
+        end = time.time()
+        return round(end - self.start, 1)
 
 
 def encode_image_to_base64(image_path):
@@ -20,11 +33,16 @@ if __name__ == '__main__':
         'max_new_tokens': 512
     }
 
+    timer = Timer()
+
     r = requests.post(
         f'{BASE_URI}/inference',
         json=payload
     )
 
+    time_taken = timer.get_elapsed_time()
+
     print(r.status_code)
     resp_json = r.json()
     print(json.dumps(resp_json, indent=4, default=str))
+    print(f'Total time taken for  API call {time_taken} seconds')
